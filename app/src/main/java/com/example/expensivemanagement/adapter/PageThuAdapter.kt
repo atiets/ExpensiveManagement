c95f74fda1e9be7b6c04379ad5657be83f167349
atiets <nguyenanhtuyet03.nbk@gmail.com>
2024-11-16 15:30:53 +0700

    crud loại thu

--- app/src/main/java/com/example/expensivemanagement/adapter/PageThuAdapter.kt
+++ app/src/main/java/com/example/expensivemanagement/adapter/PageThuAdapter.kt
@@ -2,24 +2,23 @@ package com.example.expensivemanagement.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.FragmentStatePagerAdapter
-import com.example.expensivemanagement.fragment.KhoanChiFragment
-import com.example.expensivemanagement.fragment.LoaiChiFragment
+import com.example.expensivemanagement.fragment.KhoanThuFragment
+import com.example.expensivemanagement.fragment.LoaiThuFragment
 
-class PageChiAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+class PageThuAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     override fun getItem(position: Int): Fragment = when (position) {
-        0 -> KhoanChiFragment()
-        1 -> LoaiChiFragment()
+        0 -> KhoanThuFragment()
+        1 -> LoaiThuFragment()
         else -> throw IllegalArgumentException("Vị trí không hợp lệ $position")
     }
 
     override fun getCount(): Int = 2
 
     override fun getPageTitle(position: Int): CharSequence = when (position) {
-        0 -> "Khoản Chi"
-        1 -> "Loại Chi"
+        0 -> "Khoản Thu"
+        1 -> "Loại Thu"
         else -> ""
     }
 }
