9a9dafc40844c8840bd1a041868e476e3ce1894e
caphemica <19110419@student.hcmute.edu.vn>
2024-11-22 20:33:09 +0700

    modify diag_ngansach MainActivity.kt

--- app/src/main/java/com/example/expensivemanagement/MainActivity.kt
+++ app/src/main/java/com/example/expensivemanagement/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.expensivemanagement
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
+//import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
